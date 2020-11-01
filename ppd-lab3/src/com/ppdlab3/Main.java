c372e9071dbd799c6b6b3364b01fdefc8b06bbfd
Ruxi <ruxipop00@gmail.com>
2020-11-01 21:48:22 +0200

    added some time tracking

--- ppd-lab3/src/com/ppdlab3/Main.java
+++ ppd-lab3/src/com/ppdlab3/Main.java
@@ -9,21 +9,20 @@ public class Main {
     public static void main(String[] args) {
         List<List<Integer>> matrix1 = new ArrayList<>();
         List<List<Integer>> matrix2 = new ArrayList<>();
-        int n = 3;
-        int m = 3;
+        int n = 5;
+        int m = 5;
 
-        createMatrices(matrix1, matrix2);
+        createMatrices(matrix1, matrix2, n, m);
         MatrixUtilities mu = new MatrixUtilities(matrix1, matrix2, n, m);
 
         List<Thread> threads = new ArrayList<>();
         List<List<Point>> results = new ArrayList<>();
-        int nrThreads = 3;
-        for (int i = 0; i < nrThreads; i++) {
+        for (int i = 0; i < n; i++) {
             int pos = i;
             results.add(new ArrayList<>());
-//            Thread t = new Thread(() -> mu.computePartialResultLines(pos, 0, pos, 2, results.get(pos)));
-//            Thread t = new Thread(() -> mu.computePartialResultColumns(0, pos, 2, pos, results.get(pos)));
-            Thread t = new Thread(() -> mu.computePartialResultKElement(nrThreads, pos, results.get(pos)));
+//            Thread t = new Thread(() -> mu.computePartialResultLines(pos, 0, pos, n-1, results.get(pos)));
+            Thread t = new Thread(() -> mu.computePartialResultColumns(0, pos, m-1, pos, results.get(pos)));
+//            Thread t = new Thread(() -> mu.computePartialResultKElement(nrThreads, pos, results.get(pos)));
             threads.add(t);
         }
 
@@ -33,6 +32,7 @@ public class Main {
     }
 
     public static void runWithThreads(List<Thread> threads, int n, int m) {
+        long start = System.nanoTime();
         for (Thread t : threads) {
             t.start();
         }
@@ -44,6 +44,8 @@ public class Main {
                 e.printStackTrace();
             }
         }
+        long end = System.nanoTime();
+        System.out.println("Results: " + (end - start) + ", " + threads.size() + " threads");
     }
 
     public static void runWithThreadPool(List<Thread> threads, int n, int m) {
@@ -88,7 +90,7 @@ public class Main {
         System.out.println();
     }
 
-    private static void createMatrices(List<List<Integer>> matrix1, List<List<Integer>> matrix2) {
+    private static void createFixedMatrices(List<List<Integer>> matrix1, List<List<Integer>> matrix2) {
         List<List<Integer>> dummy1 = Arrays.asList(Arrays.asList(1, 2, 3), Arrays.asList(1, 1, 2), Arrays.asList(4, 5, 2));
         List<List<Integer>> dummy2 = Arrays.asList(Arrays.asList(1, 3, 2), Arrays.asList(4, 1, 4), Arrays.asList(4, 2, 1));
 
@@ -98,6 +100,20 @@ public class Main {
             for (int j = 0; j < 3; j++) {
                 matrix1.get(i).add(dummy1.get(i).get(j));
                 matrix2.get(i).add(dummy2.get(i).get(j));
+            }
+        }
+    }
+
+    private static void createMatrices(List<List<Integer>> matrix1, List<List<Integer>> matrix2, int n, int m) {
+        int min = 10;
+        int max = 90;
+        Random rand = new Random();
+        for (int i = 0; i < n; i++) {
+            matrix1.add(new ArrayList<>());
+            matrix2.add(new ArrayList<>());
+            for (int j = 0; j < n; j++) {
+                matrix1.get(i).add(rand.nextInt((max - min) + 1) + min);
+                matrix2.get(i).add(rand.nextInt((max - min) + 1) + min);
             }
         }
     }
